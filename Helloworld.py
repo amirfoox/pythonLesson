1a2a3106b887b81767a76747993f5a08335a91e1
amirfoox@gmail.com <amirfoox@gmail.com>
2023-02-06 12:02:42 +0200

    Initial commit

---
+++ Helloworld.py
@@ -0,0 +1,10 @@
+# lesson1
+# ____________
+
+print("Hello World! ", end="")   # end="" means that it will run the two commands in the same line
+print("Hello World2!!")
+
+print("Hello World! ", "I want this to be on the same line", sep="!!! ")   # sep = separate the lines
+print("Hello World! ", "I want this to be on the same line", sep="!!! ")   # copy paste 2
+print("Hello World! ", "I want this to be on the same line", sep="!!! ")   # copy paste 3
+
